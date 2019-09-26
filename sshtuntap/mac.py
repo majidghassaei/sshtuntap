aff8f684926977be3f6baa4a99f6b3c0e98076f7
majidghassaei <majid@carrene.com>
2019-09-26 11:15:59 +0330

    Mac functions initialized for implementing

--- sshtuntap/mac.py
+++ sshtuntap/mac.py
@@ -13,11 +13,37 @@ def shell(cmd, check=True):
     print(f'Shell: {cmd}')
     return sp.run(cmd, shell=True, check=check)  #, stdout=sp.PIPE, stderr=sp.PIPE)
 
-def addroute(*args, **kwargs):
-    shell(f'ip route replace {hostaddr} via {gateway}')
-    shell(f'ip route replace default via {serveraddr}')
-    shell(
-        f'sudo -u {localuser} ssh {remoteuser}@{hostname} ' \
-        f'-Nw {index}:{index} {" ".join(sshargs)}'
-    )
+
+def replaceroute(gateway, hostaddr='default', check=True):
+#Equivalent in mac ==  return shell(f'ip route replace {hostaddr} via {gateway}', check=check)
+    pass
+
+
+def deleteroute(hostaddr, gateway):
+#Equivalent in mac ==  return shell(f'ip route del {hostaddr} via {gateway}', check=False)
+    pass
+
+
+def addtuntap(ifname, localuser):
+#Equivalent in mac ==  return shell(f'ip tuntap add mode tun dev {ifname} user ' \
+#          f'{localuser} group {localuser}')
+    pass
+
+
+def addip(ifname, clientaddr, netmask, serveraddr):
+#Equivalent in mac ==  return shell(
+#               f'ip address add dev {ifname} {clientaddr}/{netmask} ' \
+#               f'peer {serveraddr}/{netmask}'
+#           )
+    pass
+
+
+def setlink(ifname):
+#Equivalent in mac ==    return shell(f'ip link set up dev {ifname}')
+    pass
+
+
+def deletetuntap(ifname):
+#Equivalent in mac ==  return shell(f'ip tuntap delete mode tun dev {ifname}', check=False)
+    pass
 
